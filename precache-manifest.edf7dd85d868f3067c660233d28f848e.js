442acd00456ed04189044931c49bf3610a93f792
RV <rgvegajr@gmail.com>
2020-02-29 12:36:48 -0500

    Updates

---
+++ precache-manifest.edf7dd85d868f3067c660233d28f848e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4018522feb8789b381f567cb88d1185c",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "b91d0d3b8edf74347c59",
+    "url": "/clickygame/static/css/main.21349ef4.chunk.css"
+  },
+  {
+    "revision": "ecff1ac4b3f00c354d33",
+    "url": "/clickygame/static/js/2.74512566.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clickygame/static/js/2.74512566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91d0d3b8edf74347c59",
+    "url": "/clickygame/static/js/main.7e38e41a.chunk.js"
+  },
+  {
+    "revision": "4d95e025c74c73140b85",
+    "url": "/clickygame/static/js/runtime-main.2999711a.js"
+  }
+]);
